af57c9ece7046f8d4b0dfd05bfb5e8c385065e29
Daniel Chiquito <daniel.chiquito@gmail.com>
2019-02-28 23:59:01 -0500

    Added debug flag to phase 4

--- build4.sh
+++ build4.sh
@@ -3,5 +3,5 @@
 mkdir -p build/phase4
 
 echo "Building Phase 4: Reversing..."
-gcc phase4/passwordRecovery.c -o build/phase4/passwordRecovery
+gcc -g phase4/passwordRecovery.c -o build/phase4/passwordRecovery
 
